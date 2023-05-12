9615b463a932e813b8d798b812f32577ade3a3cb
NestorAlfieri <alfierinestor@gmail.com>
2023-05-11 21:00:54 -0300

    esqueleto back

---
+++ Backend/src/test/java/com/example/equipo1back/Equipo1backApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipo1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo1backApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
