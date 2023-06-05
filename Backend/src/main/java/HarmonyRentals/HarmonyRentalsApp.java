445b1427c45210b46ef75935786d4c675ea8ab5e
Leandro Acosta <leo.manuel.acosta@gmail.com>
2023-06-05 15:23:09 -0300

    Modificando endpoint del back

--- Backend/src/main/java/HarmonyRentals/HarmonyRentalsApp.java
+++ Backend/src/main/java/HarmonyRentals/HarmonyRentalsApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HarmonyRentalsApp {
-	public static final String endpoint = "http://localhost:5173";
+	public static final String endpoint = "http://buckets3-harmonyrentals-front.s3-website.us-east-2.amazonaws.com";
 
 	public static void main(String[] args) {
 
