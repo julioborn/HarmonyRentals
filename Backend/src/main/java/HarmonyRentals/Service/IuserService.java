3b4cce2f24502e13e6be54a3c4f7b638c00c60df
Leandro Acosta <leo.manuel.acosta@gmail.com>
2023-07-04 19:18:14 -0300

    Preparando ambiente para subir ultima version

--- Backend/src/main/java/HarmonyRentals/Service/IuserService.java
+++
@@ -1,9 +0,0 @@
-package HarmonyRentals.Service;
-
-import com.amazonaws.services.cognitoidp.model.SignUpRequest;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IuserService extends UserDetailsService {
-    UserDetails createUser(SignUpRequest signUpRequest);
-}
