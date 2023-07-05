e838fe1c80af5ce0c75d9eb6221301ccd529dc22
Leandro Acosta <leo.manuel.acosta@gmail.com>
2023-07-05 17:41:33 -0300

    Reemplazando ultima versión del back

--- Backend/src/main/java/HarmonyRentals/Exceptions/RentalDateConflictException.java
+++
@@ -1,7 +0,0 @@
-package HarmonyRentals.Exceptions;
-
-public class RentalDateConflictException extends RuntimeException {
-    public RentalDateConflictException(String message) {
-        super(message);
-    }
-}
