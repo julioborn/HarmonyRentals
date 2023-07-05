0115af827b0b983df15f2b7cc68fca159802bf5b
Leandro Acosta <leo.manuel.acosta@gmail.com>
2023-07-05 17:45:36 -0300

    Modificando endpoint

--- Backend/src/main/java/HarmonyRentals/HarmonyRentalsApp.java
+++ Backend/src/main/java/HarmonyRentals/HarmonyRentalsApp.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HarmonyRentalsApp {
-	public static final String endpoint = "http://localhost:5173";
-	public static final String urlLocalObackend = "localhost";
+	public static final String endpoint = "http://buckets3-harmonyrentals-front.s3-website.us-east-2.amazonaws.com/";
+	public static final String urlLocalObackend = "3.145.94.82";
 	//cambiar urlLocalObackend por "localhost" en caso de correr en forma local.
 	//cambiar urlLocalObackend por "3.145.94.82" en caso de correr en bucket.
 	//esto modifica linea 41 en AuthenticationService.
